7656e0aa8d7280a2f2c8eced11ee5fe1fa72c9b7
Abubakr Saddickq <abubakrsaddickq@gmail.com>
2023-06-08 17:05:25 -0700

    correction for advanced task

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,18 +5,18 @@ if __name__ == "__main__":
     if (len(sys.argv) == 4):
         a = int(sys.argv[1])
         b = int(sys.argv[3])
-        match sys.argv[2]:
-            case "+":
-                print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-            case "-":
-                print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
-            case "*":
-                print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
-            case "/":
-                print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
-            case _:
-                print("Unknown operator. Available operators: +, -, * and /")
-                sys.exit(1)
+        op = sys.argv[2]
+        if op == "+":
+            print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+        elif op == "-":
+            print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+        elif op == "*":
+            print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+        elif op == "/":
+            print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+        else:
+            print("Unknown operator. Available operators: +, -, * and /")
+            sys.exit(1)
     else:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
