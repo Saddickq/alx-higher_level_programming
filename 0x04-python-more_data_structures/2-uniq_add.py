3f2827ebe5cdf50aa83aa2a3aba808017d240ffb
Abubakr Saddickq <abubakrsaddickq@gmail.com>
2023-06-15 06:19:51 +0000

    .\2-uniq_add.py

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
+    sum = 0
     if my_list:
-        sum = 0
         set_list = set(my_list)
         for num in set_list:
             sum += num
+        return (sum)
+    else:
         return (sum)
