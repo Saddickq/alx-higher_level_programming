5e26dccbe07453acc8586fc8f0c17f24ae9d436c
Abubakr Saddickq <abubakrsaddickq@gmail.com>
2023-06-16 01:40:53 +0000

    12- roman numersls

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,20 @@
 #!/usr/bin/python3
+
 def roman_to_int(roman_string):
-    if roman_string:
-        roman_dict = {"I":1, "V":5, "X":10, "L":50, "C":100, "D":500}
-        
+    if not roman_string or not isinstance(roman_string, str):
+        return 0
+
+    numerals = {'M': 1000, 'D': 500, 'C': 100,
+                'L': 50, 'X': 10, 'V': 5, 'I': 1}
+
+    ans = 0
+    prev = 2000
+
+    for c in roman_string:
+        if prev < numerals[c]:
+            ans -= prev * 2
+
+        ans += numerals[c]
+        prev = numerals[c]
+
+    return ans
