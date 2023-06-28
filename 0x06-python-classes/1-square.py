b3393efc81c50b6d3e69035eed3db32c2fb279a0
Abubakr Saddickq <abubakrsaddickq@gmail.com>
2023-06-28 15:30:34 +0000

    private instance atttribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""define an class Square"""
+
+
+class Square:
+    """A class with private instance attribute"""
+
+    def __init__(self, size):
+        """Instantiation with size"""
+        self.__size = size
