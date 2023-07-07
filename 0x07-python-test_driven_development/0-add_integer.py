3037820ff4fd353dd68cc9db9874719d5719db55
Abubakr Saddickq <abubakrsaddickq@gmail.com>
2023-07-07 08:16:00 -0700

    correction

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -13,10 +13,13 @@ def add_integer(a, b=98):
         raise: TypeErrors if non-integers
         return: sum of a + b
     """
-    if not isinstance(a, (int, float)):
-        raise TypeError("a must be an integer")
-    if not isinstance(b, (int, float)):
-        raise TypeError("b must be an integer")
-    a = int(a)
-    b = int(b)
-    return (a + b)
+    try:
+        if not isinstance(a, (int, float)):
+            raise TypeError("a must be an integer")
+        if not isinstance(b, (int, float)):
+            raise TypeError("b must be an integer")
+        a = int(a)
+        b = int(b)
+        return (a + b)
+    except Exception as ex:
+        return (ex)
