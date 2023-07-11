4e70148b89521d2343d3acbe7f88fe0c9fa5b1bf
Abubakr Saddickq <abubakrsaddickq@gmail.com>
2023-07-11 16:12:37 -0700

    revert changes made to 8-rectangle

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,5 +1,27 @@
 #!/usr/bin/python3
-BaseGeometry = __import__("7-base_geometry").BaseGeometry
+"""
+A defined called BaseGeometry class
+"""
+
+
+class BaseGeometry:
+    """A BaseGeometry class"""
+
+    def area(self):
+        """
+        public instance method
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name="", value=0):
+        """
+        public instance method
+        """
+
+        if not type(value) is int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
 
 
 """ A defined class called Rectangle"""
