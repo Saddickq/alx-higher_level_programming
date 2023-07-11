7483bf11596bffcda80276ab47958f6ccdef2594
Abubakr Saddickq <abubakrsaddickq@gmail.com>
2023-07-11 07:12:26 -0700

    1-my_list.py

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+A module that inherits from a class and
+prints a list in ascending order
+"""
+
+
+class Mylist(list):
+    """
+    A class that inherits from the class list
+    """
+
+    def print_sorted(self):
+        """
+        Public instance method
+        """
+        list_copy = self.copy()
+        list_copy.sort()
+        print(list_copy)
