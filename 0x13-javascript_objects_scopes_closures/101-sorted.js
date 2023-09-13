dd86772f9e8dc4a7174a5328fc98a7fbed4ab363
Abubakr Saddickq <abubakrsaddickq@gmail.com>
2023-09-13 15:40:00 +0000

    worked on one advanced task

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,15 +1,14 @@
 #!/usr/bin/node
 const dict = require('./101-data').dict;
 
-const newDict = {};
-for (key in dict) {
-  const value = key;
-  if (!newDict[value]) {
-    newDict[value] = [];
-    newDict[value].push(key);
-  } else {
-    newDict[value].push(key);
-  }
+newDict = {};
+for (const key in dict) {
+    const value = dict[key];
+    if (newDict[value] === undefined) {
+        newDict[value] = [];
+        newDict[value].push(key);
+    } else {
+        newDict[value].push(key);
+    }
 }
-console.log(dict);
 console.log(newDict);
