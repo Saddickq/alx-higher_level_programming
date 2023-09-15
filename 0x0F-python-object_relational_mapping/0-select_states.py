0467bae46add59f62c9dc80ec5982d9f991b0a30
Abubakr Saddickq <abubakrsaddickq@gmail.com>
2023-09-15 10:52:31 -0700

    fix bugs for task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,16 +1,30 @@
 #!/usr/bin/python3
+""" A module that connect a python script to the databases hbtn_0e_0_usa """
 import MySQLdb
 from sys import argv
 
 
-if __name__ == "__main__":
+def main():
+    """ A function that selects all from the db hbtn_0e_0_usa """
     username = argv[1]
     password = argv[2]
     database = argv[3]
-    db = MySQLdb.connect(host="localhost", user=username, passwd=password, port=3306, db=database)
+    db = MySQLdb.connect(
+            host="localhost",
+            user=username,
+            passwd=password,
+            port=3306,
+            db=database
+    )
     cur = db.cursor()
-    cur.execute("SELECT * FROM states")
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
     query_rows = cur.fetchall()
 
     for row in query_rows:
         print(row)
+    cur.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
