4aa8d2571e92c61944bc6550bfbb9dd95b8f2235
Abubakr Saddickq <abubakrsaddickq@gmail.com>
2023-09-16 00:03:23 -0700

    3-my_safe_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" Write a script that takes in an argument and displays all values in the
+states table of hbtn_0e_0_usa where name matches the argument."""
+import MySQLdb
+from sys import argv
+
+
+def main():
+    """ The main function of the script"""
+    username = argv[1]
+    password = argv[2]
+    database = argv[3]
+    match = argv[4]
+
+    db = MySQLdb.connect(
+            host='localhost',
+            user=username,
+            passwd=password,
+            port=3306,
+            db=database,
+        )
+    cur = db.cursor()
+    sql = "SELECT * FROM states WHERE BINARY name LIKE %s ORDER BY id ASC"
+    cur.execute(sql, (match,))
+    query = cur.fetchall()
+    for row in query:
+        print(row)
+
+    cur.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
