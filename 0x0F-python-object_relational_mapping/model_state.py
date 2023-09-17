7e5ab88c45b490c3b159792df4658c1d8b887442
Abubakr Saddickq <abubakrsaddickq@gmail.com>
2023-09-16 20:33:00 -0700

    model_state.py

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -5,13 +5,12 @@ from sqlalchemy import create_engine, Column, String, Integer
 from sqlalchemy.ext.declarative import declarative_base
 
 
-engine = create_engine('mysql://{}:{}@localhost:{}/{}'.format("saddy", "clear", 3306, "hbtn_0e_4_usa"), pool_pre_ping=True)
-engine.connect()
 Base = declarative_base()
 
 
-class States(Base):
+class State(Base):
     """ States class that inherites from Base class """
     __tablename__ = 'states'
-    id = Column(Integer, AUTO_INCREMENT=True, PRIMARY_KEY=True, unique=True, nullable=False)
+
+    id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
