09c5462a793c63df22ab20ff1b805adff83fefb7
Abubakr Saddickq <abubakrsaddickq@gmail.com>
2023-09-30 07:39:13 -0700

    fixed pycodestyle error

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -4,15 +4,17 @@ a Python script that takes in a URL and an email, sends a POST
 request to the passed URL with the email as a parameter, and
 displays the body of the response (decoded in utf-8)
 """
-import urllib.request
-import urllib.parse
-from sys import argv
 
 
-url = argv[1]
-data = {'email': argv[2]}
-data = urllib.parse.urlencode(data).encode("ascii")
+if __name__ == '__main__':
+    import urllib.request
+    import urllib.parse
+    from sys import argv
 
-request = urllib.request.Request(url, data)
-with urllib.request.urlopen(request) as response:
-    print(response.read().decode('utf-8'))
+    url = argv[1]
+    data = {'email': argv[2]}
+    data = urllib.parse.urlencode(data).encode("ascii")
+
+    request = urllib.request.Request(url, data)
+    with urllib.request.urlopen(request) as response:
+        print(response.read().decode('utf-8'))
--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -3,15 +3,17 @@
 a Python script that takes in a URL, sends a request to the URL
 and displays the body of the response (decoded in utf-8).
 """
-import urllib
-from sys import argv
 
 
-url = argv[1]
-request = urllib.request.Request(url)
-try:
-    with urllib.request.urlopen(request) as response:
-        print(response.read())
+if __name__ == '__main__':
+    import urllib.request
+    from sys import argv
 
-except urllib.error.HTTPError as er:
-    print(f"Error code: {er.code}")
+    url = argv[1]
+    request = urllib.request.Request(url)
+    try:
+        with urllib.request.urlopen(request) as response:
+            print(response.read().decode('utf-8'))
+
+    except urllib.error.HTTPError as er:
+        print(f"Error code: {er.code}")
