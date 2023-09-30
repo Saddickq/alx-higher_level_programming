1a0e3e1ca309efcd0044e1c2bb36795615276113
Abubakr Saddickq <abubakrsaddickq@gmail.com>
2023-09-30 14:46:10 -0700

    8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -8,6 +8,15 @@ import requests
 
 
 if __name__ == '__main__':
-    char = "" if len(argv[1]) == 1 else argv[1]
-    data = {'q': char}
-    resp = requests.post('http://0.0.0.0:5000/search_user', data)
+    char = "" if len(argv) == 1 else argv[1]
+    info = {'q': char}
+    resp = requests.post('http://0.0.0.0:5000/search_user', data=info)
+
+    try:
+        json_data = resp.json()
+        if not json_data:
+            print("No result")
+        else:
+            print("[{}] {}".format(json_data.get('id'), json_data.get('name')))
+    except Exception as er:
+        print("Not a valid JSON")
