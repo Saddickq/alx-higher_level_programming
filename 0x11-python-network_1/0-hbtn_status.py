a79953b0b6e648c74858e3dd57ca7d2164a7deb0
Abubakr Saddickq <abubakrsaddickq@gmail.com>
2023-09-30 06:50:48 -0700

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,11 +2,16 @@
 """
 Write a Python script that fetches https://alx-intranet.hbtn.io/status
 """
-import urllib.request
 
 
-url = "https://alx-intranet.hbtn.io/status"
+if __name__ == '__main__':
+    import urllib.request
 
-with urllib.request.urlopen(url) as response:
-    html = response.read()
-print(html)
+    url = "https://alx-intranet.hbtn.io/status"
+    request = urllib.request.Request(url)
+    with urllib.request.urlopen(request) as response:
+        html = response.read()
+    print("Body response:")
+    print(f"\t- type: {type(html)}")
+    print(f"\t- content: {html}")
+    print(f"\t- utf8 content: {html.decode('utf8')}")
